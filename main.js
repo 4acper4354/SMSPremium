57c9e94c85bc7e7a3f53fdac0a2cc5842bc595a4
4acper4354 <132364658+4acper4354@users.noreply.github.com>
2023-08-16 17:18:16 +0200

    main.js

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ function changeFontSize() {
     if(document.getElementById("footerParagraph").style.fontSize == "11px") {
         document.getElementById("footerParagraph").innerHTML = "–––––––––––––––––––––––––––––––––––––––––––––</br>© 2023 SMSPREMIUM Sp. z o.o. Wszelkie prawa zastrzeżone.</br></br>Adres: Champ de Mars, 5 Avenue Anatole France, 75007 Paryż</br></br>OWNERS: Kacper S (49.5%) Kacper B (49.5%) Krzysztof K (1%)</br></br>Polityka prywatności | Regulamin | Kontakt</br>––––––––––––––––––––––––––––––––––––––––––––––"
     } else {
-        document.getElementById("footerParagraph").innerHTML = "––––––––––––––––---–––––––––––––––––––––––––––––––</br>© 2023 SMSPREMIUM Sp. z o.o. Wszelkie prawa zastrzeżone.</br></br>Adres: Champ de Mars, 5 Avenue Anatole France, 75007 Paryż</br></br>OWNERS: Kacper S (49.5%) Kacper B (49.5%) Krzysztof K (1%)</br></br>Polityka prywatności | Regulamin | Kontakt</br>––––––––––––––––––––---––––––––––––––––––––––––––––"
+        document.getElementById("footerParagraph").innerHTML = "––––––––––––––––------–––––––––––––––––––––––––––––––</br>© 2023 SMSPREMIUM Sp. z o.o. Wszelkie prawa zastrzeżone.</br></br>Adres: Champ de Mars, 5 Avenue Anatole France, 75007 Paryż</br></br>OWNERS: Kacper S (49.5%) Kacper B (49.5%) Krzysztof K (1%)</br></br>Polityka prywatności | Regulamin | Kontakt</br>––––––––––––––––––––------––––––––––––––––––––––––––––"
     }
 }
