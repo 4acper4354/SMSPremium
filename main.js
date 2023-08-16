91cc8966d4acaf78911fa63df2089d32b313c582
4acper4354 <132364658+4acper4354@users.noreply.github.com>
2023-08-16 18:13:17 +0200

    main.js

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-function changeFontSize() {
-    if(document.getElementById("footerParagraph").style.fontSize >= "12px") {
-        console.log("Works")
-        document.getElementById("footerParagraph").innerHTML = "––––––––––––––––––––––––––––––––––––––––––</br>© 2023 SMSPREMIUM Sp. z o.o. Wszelkie prawa zastrzeżone.</br></br>Adres: Champ de Mars, 5 Avenue Anatole France, 75007 Paryż</br></br>OWNERS: Kacper S (49.5%) Kacper B (49.5%) Krzysztof K (1%)</br></br>Polityka prywatności | Regulamin | Kontakt</br>––––––––––––––––––––––––––––––––––––––––––"
-    } else {
-        document.getElementById("footerParagraph").innerHTML = "–––––––––––––––––––––––––––––––––––––––––––––––––––––</br>© 2023 SMSPREMIUM Sp. z o.o. Wszelkie prawa zastrzeżone.</br></br>Adres: Champ de Mars, 5 Avenue Anatole France, 75007 Paryż</br></br>OWNERS: Kacper S (49.5%) Kacper B (49.5%) Krzysztof K (1%)</br></br>Polityka prywatności | Regulamin | Kontakt</br>––––––––––––––––––––––––––––––––––––––––––––––––––––––"
-    }
-}
+
